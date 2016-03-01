0ee2224da495bb2abebae0dd36de4531095d182b
Clint <wantg78@gmail.com>
2016-03-01 13:41:30 +0800

    add support in teacher info page

--- 51talk-helper.js
+++ 51talk-helper.js
@@ -1,17 +1,43 @@
 function fire() {
-    $('.teacher').each(function(index, el) {
+    $('a').each(function(index, el) {
         var el = $(el);
-        // console.log(el.data('id'));
-        $.get('/teacher/info/'+el.data('id'), function(data) {
-            var markup = data.substr(data.indexOf('人收藏')-10, 16);
-            markup = markup.substr(markup.indexOf('>')+1);
-            markup = markup.substr(0, markup.indexOf('人'));
-            console.log(markup);
-            el.find('a:eq(0)').prepend('<span style="font-size:11px;position:absolute;z-index:999;right:0;">'+markup+'</span>');
-        });
+        var matched = el.attr('href').match(/teacher\/info\/t/);//返回数组
+        if (matched != null) {
+            var tId = matched['input'].substr(matched['index']);
+            if (tId.indexOf('?')>-1) {    
+                tId = tId.substr(0, tId.indexOf('?'));
+            }
+            tId = tId.substr(tId.indexOf('/t')+2);
+            // console.log(tId);
+            loadTeacherInfo(tId, function(teacherInfo){
+                el.prepend('<span class="teacher-info">'+teacherInfo+'</span>');
+                // console.log(teacherInfo);
+            });
+        }
     });
 }
 
-if (self.location.href.indexOf('http://www.51talk.com/reserve') == 0) {
+function loadTeacherInfo(teachedId, handlerWhenFinished){
+    $.get('/teacher/info/'+teachedId, function(data) {
+        var markup = data.substr(data.indexOf('人收藏')-10, 16);
+        markup = markup.substr(markup.indexOf('>')+1);
+        markup = markup.substr(0, markup.indexOf('人'));
+        handlerWhenFinished(markup);
+    });
+}
+
+if (self.location.href.indexOf('http://www.51talk.com') == 0) {
+    $('body').append(`
+        <style>
+            .teacher-info{
+                font-size:11px;
+                position:absolute;
+                z-index:999;
+                background:#fff;
+                padding:1px;
+                border-radius:4px;
+            }
+        </style>
+    `);
     fire();
 }
